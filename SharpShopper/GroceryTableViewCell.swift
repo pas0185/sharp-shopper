ec6bfdd682b9b50586d27c37d59e7facfb07d94c
Patrick Sheehan <psheehan0301@gmail.com>
2015-02-06 16:21:17 -0600

    Moved local project to github folder

---
+++ SharpShopper/GroceryTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  GroceryTableViewCell.swift
+//  SharpShopper
+//
+//  Created by Patrick Sheehan on 2/6/15.
+//  Copyright (c) 2015 ABRAID. All rights reserved.
+//
+
+import UIKit
+
+class GroceryTableViewCell: UITableViewCell {
+
+    @IBOutlet var buySwitch: UISwitch!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var pointLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ SharpShopper/models/Grocery.swift
@@ -0,0 +1,18 @@
+//
+//  Grocery.swift
+//  SharpShopper
+//
+//  Created by Patrick Sheehan on 2/6/15.
+//  Copyright (c) 2015 ABRAID. All rights reserved.
+//
+
+import UIKit
+
+class Grocery: NSObject {
+
+    var id: Int = 0
+    var points: Int = 0
+    var name: String?
+    var price: Double = 0.00
+    
+}
