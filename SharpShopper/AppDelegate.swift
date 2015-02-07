1bd6bad762e7614d5187f146e8f092d3c735ae29
Patrick Sheehan <psheehan0301@gmail.com>
2015-02-06 19:04:11 -0600

    Removed storyboard

--- SharpShopper/AppDelegate.swift
+++ SharpShopper/AppDelegate.swift
@@ -17,9 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        let navigationController = self.window!.rootViewController as UINavigationController
-        let controller = navigationController.topViewController as MasterViewController
-        controller.managedObjectContext = self.managedObjectContext
+
+        var groceryListViewController = GroceryTableViewController()
+        self.window?.rootViewController = groceryListViewController
+
+        
+        // from template code, come back to this later
+        // groceryListViewController.managedObjectContext = self.managedObjectContext
+        
         return true
     }
 
